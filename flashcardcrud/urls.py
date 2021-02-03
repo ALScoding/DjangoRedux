a15c1ac1322f122c9b9345e3f6f2cfe709dc06fa
Alexander Schalk <Alexanderlschalk@gmail.com>
2021-02-03 12:12:12 -0600

    more work to do

--- flashcardcrud/urls.py
+++ flashcardcrud/urls.py
@@ -2,9 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from studying.views import load_view
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('api/', include('studying.api.urls')),
     path('admin/', admin.site.urls),
+    path('load/', load_view)
 ]
--- studying/views.py
+++ studying/views.py
@@ -7,22 +7,24 @@ from django.shortcuts import render
 # relative import of forms
 from .models import Flashcard
 from django.urls import path
+from django.http import HttpResponse
 # Create your views here.
 
 def load_view(request):
     context = {}
     # loads flashcards.json and creates objects in the database
     # utf-8 encoding fixes problem with using Japanese characters
-    with open("flashcards.json", "r", encoding="utf-8") as file:
-        data = file.read()
-        json_data = json.loads(data)
-        context["data"] = "Flash Cards Loaded Successfully"
+    if Flashcard.objects.count() == 0:
+        with open("flashcards.json", "r", encoding="utf-8") as file:
+            data = file.read()
+            json_data = json.loads(data)
+            context["data"] = "Flash Cards Loaded Successfully"
 
-        for flashcard in json_data.get("cards"):
-            Flashcard.objects.create(
-                backside=flashcard.get("backside"),
-                frontside=flashcard.get("frontside"),
-                answer=flashcard.get("answer")
-            )
+            for flashcard in json_data.get("cards"):
+                Flashcard.objects.create(
+                    backside=flashcard.get("backside"),
+                    frontside=flashcard.get("frontside"),
+                    answer=flashcard.get("answer")
+                )
 
-    return render(request, "load_view.html", context) 
+    return HttpResponse('data loaded')
