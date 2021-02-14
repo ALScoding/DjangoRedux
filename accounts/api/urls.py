042259d36b9e4984fb2b219d9d121cdd155e2eca
Alexander Schalk <Alexanderlschalk@gmail.com>
2021-02-13 22:35:34 -0600

    adding tests

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('', include('knox.urls')),
     path('user', UserAPIView.as_view()),
     path('register', RegisterAPIView.as_view()),
-    path('login', LoginAPIView.as_view()),
+    path('login', LoginAPIView.as_view(), name='login'),
     # path('logout', LogoutView.as_view(), name='knox_logout')
 ]
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from django.urls import reverse
+from accounts.api.views import LoginAPIView
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'user',
+            'password': 'password'
+        }
+        user = User.objects.create_user(**self.credentials)
+        self.token = Token.objects.create(user = user)
+    def test_login(self):
+        # send login data
+        path = reverse('login')
+        data={'username': 'user', 'password': 'password'}
+        request=RequestFactory().post(path, data, content_type='application/json')
+        # should be logged in now
+        view=LoginAPIView.as_view()
+        response=view(request)
+        # print(response.__dict__)
+        assert response.status_code == 200
+        assert response.data == {'user': {'id': 1, 'username': 'user', 'email': ''}, 'token': self.token.key}
